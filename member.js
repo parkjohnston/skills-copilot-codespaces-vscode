ccdd4fb1ef5c2d1f117e68d37f5ab12d6ca77e53
Parker Johnston <parkjohnston@gmail.com>
2024-09-19 18:18:06 +0000

    Copilot Second Commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  let member = {
+    name: 'John',
+    age: 25,
+    skills: ['HTML', 'CSS', 'JavaScript']
+  };
+
+  console.log(`${member.name} has skills: ${member.skills.join(', ')}`);
+}
